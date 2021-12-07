f954555ec4499288291fd9016bbf4f800afa01c2
nikita.knoefel@gmail.com <nikita.knoefel@gmail.com>
2021-12-07 17:02:15 +0100

    0.7 release

--- Source/ClimateCycleExtendedModSettings.cs
+++ Source/ClimateCycleExtendedModSettings.cs
@@ -37,19 +37,20 @@ namespace ClimateCycleExtended
         {
             Listing_Standard listing_Standard = new Listing_Standard();
 
+            int durationInDays = (int)settings.cyclePeriods * 60;
+            int daysToHotPeak = settings.inverted ? durationInDays - durationInDays / 4 : durationInDays / 4;
+            int daysToColdPeak = settings.inverted ? durationInDays / 4 : durationInDays - durationInDays / 4;
+
             listing_Standard.Begin(inRect);
             
             listing_Standard.AddLabeledNumericalTextField<float>("CCE_ModSettings_OffsetHot".Translate(), ref settings.temperatureOffsetWarm, minValue: 0f, maxValue: 250f);
             listing_Standard.AddLabeledNumericalTextField<float>("CCE_ModSettings_OffsetCold".Translate(), ref settings.temperatureOffsetCold, minValue: 0f, maxValue: 250f);
             listing_Standard.AddLabeledNumericalTextField<float>("CCE_ModSettings_CycleDuration".Translate(), ref settings.cyclePeriods, minValue: 1f, maxValue: 1000f);
             listing_Standard.CheckboxLabeled("CCE_ModSettings_InvertCycle".Translate(), ref settings.inverted, "CCE_ModSettings_InvertCycle_ToolTip".Translate());
-
-            int durationInDays = (int)settings.cyclePeriods * 60;
-            int daysToHotPeak = settings.inverted ? durationInDays - durationInDays / 4 : durationInDays / 4;
-            int daysToColdPeak = settings.inverted ?durationInDays / 4 : durationInDays - durationInDays / 4;
-            listing_Standard.Label("The cycle will last " + settings.cyclePeriods + " years (" + durationInDays + "days). The first hot peak will reach its limit after " + daysToHotPeak + " days. And the first cold peak after " + daysToColdPeak + " days.");
+            listing_Standard.Label("CCE_ModSettings_InfoTextA".Translate() + settings.cyclePeriods + "CCE_ModSettings_InfoTextB".Translate() + durationInDays + "CCE_ModSettings_InfoTextC".Translate() + daysToHotPeak + "CCE_ModSettings_InfoTextD".Translate() + daysToColdPeak + "CCE_ModSettings_InfoTextE".Translate());
+            
             listing_Standard.End();
-            //"The cycle will last " + + " years (" + + "days). The first hot peak will reach its limit after " + daysToHotPeak + " days. And the first cold peak after " + daysToColdPeak + " days."
+
             settings.Write();
         }
     }
